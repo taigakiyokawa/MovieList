d7eb975069d931bf4cba9581a33ffe23a3e0e0ff
Taiga KIYOKAWA <taiga.kiyokawa106@mail.kyutech.jp>
2018-09-11 01:48:19 +0900

    Initial Commit

---
+++ MovieList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieList
+//
+//  Created by Taiga KIYOKAWA on 2018/09/11.
+//  Copyright © 2018年 Taiga KIYOKAWA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
